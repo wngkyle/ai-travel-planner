d19e890925dde90ff8b5d164da46c5bec293f977
wngkyle <99611120+wngkyle@users.noreply.github.com>
2023-11-01 08:10:11 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Heading from "./components/Heading"
+import Body from "./components/Body"
+
+
+function App() {
+
+  return (
+    <>
+      <Heading />
+      <Body />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { Provider } from 'redux'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
